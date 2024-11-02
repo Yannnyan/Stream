87bf79d00f32f6c70e934f4d79fd04aa84c0ce48
Yan <regoner2@gmail.com>
2024-11-03 01:44:21 +0200

    first commit

---
+++ src/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using LibVLCSharp.Shared;
+
+namespace src
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            using var libvlc = new LibVLC(enableDebugLogs: true);
+            using var media = new Media(libvlc, new Uri(@"C:\Users\Yan\Desktop\Music\2011 summer vibes nostalgia playlist.mp4"));
+            using var mediaplayer = new LibVLCSharp.Shared.MediaPlayer(media);
+
+            mediaplayer.Play();
+
+            Console.ReadKey();
+        }
+    }
+
+}
